21acd172ba2b065a509598de3c3927e0afe78c77
Zefeng Zhang <44664641+OldWilson@users.noreply.github.com>
2022-10-29 12:04:16 +0800

    update

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,10 @@
 package org.example;
 
-public class Main {
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@SpringBootApplication
+public class Main extends WebMvcConfigurerAdapter {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
---
+++ src/main/java/org/example/temp/CountTest.java
@@ -0,0 +1,16 @@
+package org.example.temp;
+
+public class CountTest {
+
+    public static void main(String[] args) {
+        // 1. 3000
+        // 3000 - 5 * 200 - 3 * 333 - 467
+    }
+
+    public int getNum(int num, int distance) {
+        int times = num % 1000 * 2 - 1;
+
+
+        return 0;
+    }
+}
