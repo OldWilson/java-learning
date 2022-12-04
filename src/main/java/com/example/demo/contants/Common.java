e0f4116ca49d82b10e6c7762f04b063b4547cc70
zefeng <chixyosan@outlook.com>
2022-12-04 09:00:45 +0800

    update

---
+++ src/main/java/com/example/demo/contants/Common.java
@@ -0,0 +1,23 @@
+package com.example.demo.contants;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Common {
+    public static Map<String, String> FULL_ANGLE_DIGITAL = new HashMap<>();
+    static {
+        Common.FULL_ANGLE_DIGITAL.put("１", "1");
+        Common.FULL_ANGLE_DIGITAL.put("２", "2");
+        Common.FULL_ANGLE_DIGITAL.put("３", "3");
+        Common.FULL_ANGLE_DIGITAL.put("４", "4");
+        Common.FULL_ANGLE_DIGITAL.put("５", "5");
+        Common.FULL_ANGLE_DIGITAL.put("６", "6");
+        Common.FULL_ANGLE_DIGITAL.put("７", "7");
+        Common.FULL_ANGLE_DIGITAL.put("８", "8");
+        Common.FULL_ANGLE_DIGITAL.put("９", "9");
+        Common.FULL_ANGLE_DIGITAL.put("０", "0");
+        Common.FULL_ANGLE_DIGITAL.put("　", "");
+        Common.FULL_ANGLE_DIGITAL.put(" ", "");
+    }
+
+}
--- src/test/java/com/example/demo/controller/ut/WelcomeControllerTest.java
+++ src/test/java/com/example/demo/controller/ut/WelcomeControllerTest.java
@@ -1,5 +1,6 @@
 package com.example.demo.controller.ut;
 
+import com.example.demo.contants.Common;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -8,12 +9,14 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import java.util.Map;
+
 import static org.hamcrest.Matchers.equalTo;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest
-@AutoConfigureMockMvc
+//@SpringBootTest
+//@AutoConfigureMockMvc
 public class WelcomeControllerTest {
 
     @Autowired
@@ -24,5 +27,19 @@ public class WelcomeControllerTest {
         mvc.perform(MockMvcRequestBuilders.get("/welcome").accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andExpect(content().string(equalTo("welcome to docker")));
+    }
+
+    @Test
+    public void test001() {
+        String str = "１２３４５６７８９　０";
+        for (Map.Entry<String, String> entry : Common.FULL_ANGLE_DIGITAL.entrySet()) {
+            String key = entry.getKey();
+            String value = entry.getValue();
+            if (str.contains(key)) {
+                str = str.replace(key, value);
+            }
+        }
+        System.out.println(str);
+        System.out.println();
     }
 }
